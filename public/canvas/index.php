d8f1790a58003109e0b9652bda58fbde39881575
Tony Findeisen <github@it-ony.de>
2012-12-21 21:10:29 +0100

    bootstrapping

--- public/canvas/index.php
+++ public/canvas/index.php
@@ -1,7 +1,52 @@
 <?php
 session_start();
 
-echo "<pre>" . print_r($_REQUEST) . "</pre>";
+$config = array(
+    "appId" => "206748646126990",
+    "secret" => ""
+);
 
+require_once "lib/sprd/controller/CanvasController.php";
+$shopSetup = new CanvasController($config);
+$user = $shopSetup->handleRequest();
 
 ?>
+<!DOCTYPE HTML>
+<html>
+<head>
+    <!--
+    Created with rAppid.js - a declarative RIA MVC JS Framework developed by Tony Findeisen and Marcus Krejpowicz
+    Visit http://rappidjs.com for more Information.
+    -->
+    <title>Spreadshirt</title>
+
+    <!-- VERSION="${VERSION}" -->
+
+    <link rel="stylesheet" href="installer/app/css/bootstrap.min.css"/>
+    <link rel="stylesheet" href="installer/app/css/installer.css"/>
+
+    <script type="text/javascript" src="installer/js/lib/require.js" data-usage="lib"></script>
+    <script type="text/javascript" src="installer/js/lib/rAppid.js" data-usage="lib"></script>
+
+    <meta name="viewport" content="initial-scale=1, maximum-scale=1, user-scalable=0">
+    <meta name="apple-mobile-web-app-capable" content="yes">
+
+    <meta name="fragment" content="!">
+
+    <!-- Le HTML5 shim, for IE6-8 support of HTML5 elements -->
+    <!--[if lt IE 9]>
+    <script src="//html5shim.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+</head>
+<body>
+<script type="text/javascript" data-usage="bootstrap">
+    rAppid.bootStrap("app/FbInstaller.xml", document.body, {
+        language: "<?php echo $user->getLanguage(); ?>",
+        region: "<?php echo $user->getRegion(); ?>"
+    }, {
+        baseUrl: "installer",
+        loadConfiguration: "config.json"
+    });
+</script>
+</body>
+</html>
