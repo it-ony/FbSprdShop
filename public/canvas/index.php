abf4e82bd6e6cb50fd067fc784ab83fa2b5d6d8f
Tony Findeisen <github@it-ony.de>
2012-12-29 17:28:11 +0100

    use FB.ui to add app to facebook page

--- public/canvas/index.php
+++ public/canvas/index.php
@@ -1,52 +1,54 @@
-<?php
-session_start();
-
-$config = array(
-    "appId" => "206748646126990",
-    "secret" => ""
-);
-
-require_once "lib/sprd/controller/CanvasController.php";
-$shopSetup = new CanvasController($config);
-$user = $shopSetup->handleRequest();
-
-?>
 <!DOCTYPE HTML>
 <html>
 <head>
-    <!--
-    Created with rAppid.js - a declarative RIA MVC JS Framework developed by Tony Findeisen and Marcus Krejpowicz
-    Visit http://rappidjs.com for more Information.
-    -->
-    <title>Spreadshirt</title>
-
-    <!-- VERSION="${VERSION}" -->
-
-    <link rel="stylesheet" href="installer/app/css/bootstrap.min.css"/>
-    <link rel="stylesheet" href="installer/app/css/installer.css"/>
-
-    <script type="text/javascript" src="installer/js/lib/require.js" data-usage="lib"></script>
-    <script type="text/javascript" src="installer/js/lib/rAppid.js" data-usage="lib"></script>
-
-    <meta name="viewport" content="initial-scale=1, maximum-scale=1, user-scalable=0">
-    <meta name="apple-mobile-web-app-capable" content="yes">
-
-    <meta name="fragment" content="!">
-
-    <!-- Le HTML5 shim, for IE6-8 support of HTML5 elements -->
-    <!--[if lt IE 9]>
-    <script src="//html5shim.googlecode.com/svn/trunk/html5.js"></script>
-    <![endif]-->
+    <title>Add SprdShop to your page</title>
 </head>
 <body>
-<script type="text/javascript" data-usage="bootstrap">
-    rAppid.bootStrap("app/FbInstaller.xml", document.body, {
-        language: "<?php echo $user->getLanguage(); ?>",
-        region: "<?php echo $user->getRegion(); ?>"
-    }, {
-        baseUrl: "installer",
-        loadConfiguration: "config.json"
-    });
-</script>
+    <div id='fb-root'></div>
+    <script src='//connect.facebook.net/en_US/all.js'></script>
+
+    <div id="step1">
+        <h1>Install SprdShop to your page</h1>
+        <a href="javascript:addToPage();">Add</a>
+    </div>
+
+    <div id="step2" style="display: none">
+        <h1>Installed on your page.</h1>
+    </div>
+
+    <script>
+
+        var appId = "206748646126990";
+
+        FB.init({
+            appId: appId,
+            status: true,
+            cookie: true
+        });
+
+        function addToPage() {
+
+            FB.ui({
+                method: 'pagetab'
+            }, function(response) {
+
+                if (response && response.tabs_added ) {
+                    for (var key in response.tabs_added) {
+                        if (key in response.tabs_added) {
+                            // redirect to page
+                            var url = "//www.facebook.com/" + key + "?sk=app_" + appId;
+
+                            document.getElementById("step1").style.display = "none";
+                            document.getElementById("step2").style.display = "block";
+
+                            window.open(url, "", "resizable=yes,scrollbars=yes,status=yes", true);
+
+                        }
+                    }
+                }
+            });
+        }
+
+    </script>
 </body>
 </html>
