15ee1fd031b1ea8440e7314e7f4f12fe905e7780
Tony Findeisen <github@it-ony.de>
2012-12-29 17:30:10 +0100

    user model to retrieve user information

---
+++ public/lib/sprd/model/User.php
@@ -0,0 +1,65 @@
+<?php
+/**
+ * User: tony
+ * Date: 21.12.12
+ * Time: 15:35
+ */ 
+class User {
+
+    /***
+     * @var Array
+     */
+    protected $data;
+    protected $userId;
+
+    function __construct($data, $userId)
+    {
+        $this->data = $data;
+        $this->userId = $userId;
+    }
+
+    /***
+     * @return string
+     */
+    public function getCountry() {
+        return $this->data["country"];
+    }
+
+    /***
+     * @return string
+     */
+    public function getLocale()
+    {
+        return $this->data["locale"];
+    }
+
+    /***
+     * @return string
+     */
+    public function getLanguage() {
+        return substr($this->getLocale(), 0, 2);
+    }
+
+    /***
+     * @return string
+     */
+    public function getUserId()
+    {
+        return $this->userId;
+    }
+
+    /***
+     * @return string
+     */
+    public function getRegion()
+    {
+        $language = $this->getCountry();
+
+        if (in_array($language, array("us"))) {
+            return "na";
+        }
+
+        return "eu";
+    }
+
+}
